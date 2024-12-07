5051e92ce5dea43e3cf1e66a322a4921ba6f5847
qx3kk <romanchuk.up@gmail.com>
2024-12-07 02:14:53 +0200

    feat: додай серіалізацію полів із модулів

--- api/views.py
+++ api/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from .models import Translation, User, Vocabulary, WordPair
+from .serializers import TranslationSerializer, UserSerializer, VocabularySerializer, WordPairSerializer
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class VocabularyViewSet(ModelViewSet):
+    queryset = Vocabulary.objects.all()
+    serializer_class = VocabularySerializer
+
+
+
+
+class WordPairViewSet(ModelViewSet):
+    queryset = WordPair.objects.all()
+    serializer_class = WordPairSerializer
+
+
+
+class TranslationViewSet(ModelViewSet):
+    queryset = Translation.objects.all()
+    serializer_class = TranslationSerializer
